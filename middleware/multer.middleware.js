698b1755ff70bfa4e047d2d8863c6480e0c8ab33
Rashmi Ranjan Behera <110801220+Rashmi7205@users.noreply.github.com>
2023-09-15 11:09:41 +0530

    Update multer.middleware.js

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -2,27 +2,27 @@ import path from 'path';
 import multer from 'multer';
 
 const upload = multer({
-    dest:'uploads',
-    limits:{fileSize:5*1024*1024}, //max size 5 mb
-    storage:multer.diskStorage({
-        destination:'/uploads/',
-        filename:(_req,file,cb)=>{
-            cb(null,file.originalname);
-        }
+    dest: path.join(__dirname, '../uploads'), // Use a relative path
+    limits: { fileSize: 5 * 1024 * 1024 }, // Max size 5 MB
+    storage: multer.diskStorage({
+        destination: path.join(__dirname, '../uploads'), // Use a relative path
+        filename: (_req, file, cb) => {
+            cb(null, file.originalname);
+        },
     }),
-    fileFilter:(_req,file,cb)=>{
-        let ext = path.extname(file.originalname);        
-        if(
-            ext!==".jpg" &&
-            ext!==".png" &&
-            ext!==".webp" &&
-            ext!==".jpeg" &&
-            ext!==".mp4"
-        ){
-            cb(new Error(`Unsupported File Format type ${ext}`),false);
+    fileFilter: (_req, file, cb) => {
+        let ext = path.extname(file.originalname);
+        if (
+            ext !== '.jpg' &&
+            ext !== '.png' &&
+            ext !== '.webp' &&
+            ext !== '.jpeg' &&
+            ext !== '.mp4'
+        ) {
+            cb(new Error(`Unsupported File Format type ${ext}`), false);
             return;
         }
-        cb(null,true);
+        cb(null, true);
     },
 });
 
