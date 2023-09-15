14d7a4c58e186357f4aeecbd2dc78a8c12778467
Rashmi Ranjan Behera <110801220+Rashmi7205@users.noreply.github.com>
2023-09-15 11:14:16 +0530

    Update multer.middleware.js

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,6 +1,9 @@
 import path from 'path';
 import multer from 'multer';
 
+// Get the directory name of the current module
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const upload = multer({
     dest: path.join(__dirname, '../uploads'), // Use a relative path
     limits: { fileSize: 5 * 1024 * 1024 }, // Max size 5 MB
@@ -27,3 +30,4 @@ const upload = multer({
 });
 
 export default upload;
+
