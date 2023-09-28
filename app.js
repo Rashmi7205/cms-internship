9198e19a499791cf76476732df4d2713449e1f7c
Rashmi Ranjan Behera <110801220+Rashmi7205@users.noreply.github.com>
2023-09-28 11:12:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-import  express  from "express";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
-import blogRoutes from './routes/blogs.routes.js'
-import errorMiddleWare from './middleware/error.middleware.js';
-
+import blogRoutes from './routes/blogs.routes.js';
+import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
-app.use(express.json());
+
+// Enable CORS with credentials
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
+// Middleware for parsing cookies, JSON, and URL-encoded data
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-                origin: 'http://localhost:5173',
-          credentials: true, }
-            ));
-
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
-
-app.use('/api/v1/user',userRoutes);
-app.use('/api/v1/blog',blogRoutes);
-
-app.all('*',(req,res)=>{
-    return res.status(400).send('OOPS!! PAGE NOT FOUND');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blog', blogRoutes);
 
+app.all('*', (req, res) => {
+  return res.status(400).send('OOPS!! PAGE NOT FOUND');
+});
 
-app.use(errorMiddleWare);
+app.use(errorMiddleware);
 
-export default app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
